58c35f5e5d46292605c8da6d33d81bc65ef69524
Sivat <979192956@qq.com>
2018-11-26 22:09:19 +0800

    住客时间段视图

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -29,9 +29,7 @@
                         <el-button v-else class="flr" type='primary' @click="handleReply">
                                     回复
                         </el-button>
-                      
                   </div>
-                    
                </div>
            </el-card>
        </div>
@@ -75,8 +73,33 @@
                         <span class="title">住客</span>
                         <el-button style="float: right; padding: 3px 0" type="text">查看全部<i class="iconfont icon-arw-top-copy"></i></el-button>
                     </div>
-                    <div class="progress">
-
+                    <div class="progress clearfix" >
+                        <div class="inline arriveHotelTime"> 
+                            <span class="arriveHotelTimeSpan">5月12日</span> 
+                            <br/>
+                            <span class="arriveHotelTimeTitleSpan">预计到店时间</span>
+                        </div>
+                         <div class="inline timeItem"> 
+                            <hr size=1 class="dottedLine">     
+                            <div  v-for="i in 2" :key="i" class="inline inlineTimeItem"  >
+                                <div v-if="i == 1" class="beginTime">
+                                    <span class="inline inlineTimeItemSpan">09:30</span> 
+                                    <br/>
+                                    <img class="circle">
+                                </div>
+                                <div v-if="i == 2" class="endTime">
+                                    <span class="inline inlineTimeItemSpan">12:10</span>
+                                    <br/>
+                                    <img class="circle"> 
+                                </div>
+                            </div>
+                        </div>
+                        <div class="inline rightTitlt"> 
+                            <span class="arriveHotelTimeSpan">仅显示</span> 
+                            <br/>
+                            <span class="arriveHotelTimeSpan">三条信息</span>
+                        </div>
+                        
                     </div>
                     <div v-for="o in 3" :key="o" class="text orderitem">      
                         <div class="auditLeave-top ">
@@ -276,8 +299,65 @@ let stars = document.getElementById('stars')
 .carditem{ margin-bottom: 10px;}
 .title{ padding-left: 6px;border-left:3px solid #75b8fc;}
 .header{font-weight: 700;}
+
+.progress{
+    margin-bottom: 10px;
+    .inline{
+        float:left;
+    }
+    .arriveHotelTime{
+     .arriveHotelTimeSpan{
+       color:#409eff;
+       font-size: 15px;
+     }
+     .arriveHotelTimeTitleSpan{
+       font-size: 15px;
+     }
+    }
+    .rightTitlt{
+        float: right;
+        color:#409eff;
+        font-size: 15px;
+    }
+    .timeItem{
+        width: 75%;
+        height: 40px; 
+    }
+    .dottedLine {
+     color: #409eff;
+     border-style: dashed;
+     margin-left: 20px;
+     margin-top: 25px;
+    }
+    .inlineTimeItem{
+        position: relative;
+    }
+    .inlineTimeItemSpan{
+        color: #409eff;
+    }
+    .circle {
+        background-color: #409eff;
+        height: 15px;
+        width: 15px;
+        border-radius: 7.5px;
+        margin-top: 10px;
+        margin-left: 12px;
+    }
+    .beginTime{
+        position: absolute;
+        top: -33px;
+    }
+    .endTime{
+        position: absolute;
+        right: -420px;
+        top: -33px;
+    }
+}
+
+
  
 </style>
+
 <style>
  .el-main{
     text-align: left;
